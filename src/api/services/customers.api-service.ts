d5fd768604ccdd6b97726c22678e01921e2be623
JosieVi <o.s.marushkina@gmail.com>
2025-06-13 22:11:26 +0300

    fixed an issue in the controller for deleting a customer

--- src/api/services/customers.api-service.ts
+++ src/api/services/customers.api-service.ts
@@ -53,7 +53,6 @@ export class CustomersApiService {
     @logStep("Delete Customer by ID via API")
     async deleteCustomer(id: string, token: string) {
         const response = await this.controller.delete(id, token);
-        console.log(`response: ${JSON.stringify(response)}`)
         validateResponse(response, STATUS_CODES.DELETED, null, null);
     }
 

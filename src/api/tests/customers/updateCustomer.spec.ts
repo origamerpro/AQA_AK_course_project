ccebd0a2a04582f674c41bf681b2872d2e28c250
JosieVi <o.s.marushkina@gmail.com>
2025-06-15 04:05:56 +0300

    Add tests for endpoint PUT /api/customers/id

---
+++ src/api/tests/customers/updateCustomer.spec.ts
@@ -0,0 +1,89 @@
+import { test } from "fixtures/api-services.fixture";
+import { generateCustomerData } from "data/customers/generateCustomer.data";
+import { TAGS } from "data/testTags.data";
+import { validateResponse } from "utils/validations/responseValidation";
+import { negativeTestCasesForUpdate, negativeTestCasesForUpdateWithoutToken, positiveTestCasesForUpdate } from "data/customers/updateCustomerCases.data";
+import { ICustomer } from "types/customer.types";
+import { STATUS_CODES } from "data/statusCodes";
+
+test.describe("[API] [Customers] Update the customer by ID", () => {
+
+    let token = "";
+    let originalCustomerID = "";
+    let originalCustomerData: ICustomer = {} as ICustomer;
+
+    test.beforeEach(async ({ signInApiService, customersApiService }) => {
+        token = await signInApiService.loginAsLocalUser();
+        originalCustomerData = generateCustomerData();
+        const originalCustomer = await customersApiService.createCustomer(token, originalCustomerData);
+        originalCustomerID = originalCustomer._id;
+    });
+
+    test.afterEach(async ({ customersApiService }) => {
+        await customersApiService.deleteCustomer(originalCustomerID, token);
+    });
+
+
+    test.describe("Positive", () => {
+
+        positiveTestCasesForUpdate.forEach(({ name, newCustomerData, expectedStatusCode, isSuccess, errorMessage }) => {
+            test(`Should update customer: ${name}`,
+                { tag: [TAGS.API, TAGS.CUSTOMERS, TAGS.SMOKE, TAGS.REGRESSION] },
+                async ({ customersController }) => {
+
+                    const response = await customersController.update(originalCustomerID, { ...originalCustomerData, ...newCustomerData }, token);
+                    validateResponse(response, expectedStatusCode, isSuccess, errorMessage);
+                })
+        });
+    });
+
+    test.describe("Negative", () => {
+
+        negativeTestCasesForUpdateWithoutToken.forEach(({ name, newCustomerData, token, expectedStatusCode, isSuccess, errorMessage }) => {
+            test(`Should NOT update customer: ${name}`,
+                { tag: [TAGS.API, TAGS.CUSTOMERS, TAGS.REGRESSION] },
+                async ({ customersController }) => {
+
+                    const response = await customersController.update(originalCustomerID, { ...originalCustomerData, ...newCustomerData }, token);
+                    validateResponse(response, expectedStatusCode, isSuccess, errorMessage);
+                })
+        });
+
+        negativeTestCasesForUpdate.forEach(({ name, newCustomerData, expectedStatusCode, isSuccess, errorMessage }) => {
+            test(`Should NOT update customer: ${name}`,
+                { tag: [TAGS.API, TAGS.CUSTOMERS, TAGS.REGRESSION] },
+                async ({ customersController }) => {
+
+                    const response = await customersController.update(originalCustomerID, { ...originalCustomerData, ...newCustomerData }, token);
+                    validateResponse(response, expectedStatusCode, isSuccess, errorMessage);
+                })
+        });
+
+        test("Should NOT update customer: Duplicate email",
+            { tag: [TAGS.API, TAGS.CUSTOMERS, TAGS.REGRESSION] },
+            async ({ customersController, customersApiService }) => {
+
+                const dublicateCustomerData = generateCustomerData();
+                const dublicateCustomer = await customersApiService.createCustomer(token, dublicateCustomerData);
+                const dublicateCustomerID = dublicateCustomer._id;
+
+                const response = await customersController.update(originalCustomerID, { ...originalCustomerData, email: dublicateCustomerData.email }, token);
+                validateResponse(response, STATUS_CODES.CONFLICT, false, `Customer with email '${dublicateCustomerData.email}' already exists`);
+
+                await customersApiService.deleteCustomer(dublicateCustomerID, token);
+            });
+
+        test("Should NOT update customer: ID of non-existent customer",
+            { tag: [TAGS.API, TAGS.CUSTOMERS, TAGS.REGRESSION] },
+            async ({ customersController, customersApiService }) => {
+
+                const secondCustomerData = generateCustomerData();
+                const secondCustomer = await customersApiService.createCustomer(token, secondCustomerData);
+                const secondCustomerID = secondCustomer._id;
+                await customersApiService.deleteCustomer(secondCustomerID, token);
+
+                const response = await customersController.update(secondCustomerID, generateCustomerData(), token);
+                validateResponse(response, STATUS_CODES.NOT_FOUND, false, `Customer with id '${secondCustomerID}' wasn't found`);
+            });
+    });
+});

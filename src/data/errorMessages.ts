88053125f76a4562b341245f10b833613353d5e3
SoloweyAnd <soloweykoandrey@gmail.com>
2025-06-15 09:40:56 +0300

    add GET /api/customers/id endpoint tests

--- src/data/errorMessages.ts
+++ src/data/errorMessages.ts
@@ -2,4 +2,5 @@ export const ERROR_MESSAGES = {
   INCORRECT_REQUEST_BODY: 'Incorrect request body',
   NOT_AUTHORIZED: 'Not authorized',
   INVALID_ACCESS_TOKEN: 'Invalid access token',
+  CUSTOMER_NOT_FOUND: (id: string) => `Customer with id '${id}' wasn't found`,
 };

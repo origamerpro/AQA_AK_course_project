1b2a0db1e5d2579286d8da5322431b99a5565182
JosieVi <o.s.marushkina@gmail.com>
2025-06-28 15:45:51 +0300

    Add PO for Orders List

---
+++ src/data/orders/ordersListColumn.data.ts
@@ -0,0 +1,10 @@
+export enum OrdersListColumn {
+  OrderNumber = 'Order Number',
+  Email = 'Email',
+  Price = 'Price',
+  Delivery = 'Delivery',
+  Status = 'Status',
+  AssignedManager = 'Assigned Manager',
+  CreatedOn = 'Created On',
+  Actions = 'Actions',
+}

9010cb2e1b81b3e944cd72f9ddc4906101dc6a93
bshamukou <mr.shamukov@gmail.com>
2025-06-30 23:40:39 +0200

    Add latest fix for comments

--- src/ui/pages/modals/orders/editCustomerModal.page.ts
+++ src/ui/pages/modals/orders/editCustomerModal.page.ts
@@ -2,8 +2,9 @@ import { BaseModal } from '../baseModal.page';
 import { logStep } from 'utils/reporter.utils';
 
 export class EditCustomerModalPage extends BaseModal {
-  readonly uniqueElement = this.page.locator('.modal-header h5');
+  readonly uniqueElement = this.page.locator('#edit-customer-modal .modal-content');
 
+  readonly modalTitle = this.uniqueElement.locator('h5');
   readonly customersList = this.uniqueElement.locator('select[name="Customer"]',);
   readonly saveButton = this.uniqueElement.locator('#update-customer-btn');
   readonly cancelButton = this.uniqueElement.locator('#cancel-edit-customer-modal-btn',);
@@ -11,7 +12,7 @@ export class EditCustomerModalPage extends BaseModal {
 
   @logStep('Get modal title')
   async getModalTitle() {
-    return await this.uniqueElement.innerText();
+    return await this.modalTitle.innerText();
   }
 
   @logStep('Select another customer for order')

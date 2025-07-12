9bffeae9d5726ca2df4d2f98cba9b206566b0ef4
bshamukou <mr.shamukov@gmail.com>
2025-07-12 20:23:08 +0200

    Fix with comments

--- src/api/tests/orders/getOrderById.spec.ts
+++ src/api/tests/orders/getOrderById.spec.ts
@@ -1,104 +1,57 @@
-import { test } from 'fixtures/api-services.fixture';
+import { test } from 'fixtures/ordersCustom.fixture';
 import { STATUS_CODES } from "data/statusCodes";
 import { TAGS } from "data/testTags.data";
 import { validateResponse } from "utils/validations/responseValidation";
+import { validateSchema } from '../../../utils/validations/schemaValidation';
+import { getOrderByIDResponseSchema } from 'data/schemas/order.schema';
+import { MOCK_PRODUCT_ONE } from '../../../data/orders/mockOrders.data';
+import { ERROR_MESSAGES } from '../../../data/errorMessages';
 
 test.describe('[API] [Orders] Get order by id', () => {
   let token = '';
-  const createdOrderIds: string[] = [];
-  const createdProductIds: string[] = [];
-  const createdCustomerIds: string[] = [];
 
   test.beforeEach(async ({ signInApiService }) => {
     token = await signInApiService.loginAsLocalUser();
   });
 
-  test.afterAll(async ({ dataDisposalUtils }) => {
-    await dataDisposalUtils.clearOrders(createdOrderIds);
-    await dataDisposalUtils.clearCustomers(createdCustomerIds);
-    await dataDisposalUtils.clearProducts(createdProductIds);
-  });
-
   test.describe('Positive', () => {
     test('200 OK - Get order by id',
       { tag: [TAGS.API, TAGS.ORDERS, TAGS.SMOKE] },
-      async ({ ordersController, customersApiService, productsApiService }) => {
-        const customer = await customersApiService.createCustomer(token);
-        const product = await productsApiService.create(token);
-        const data = {
-          customer: customer._id,
-          products: [product._id],
-        };
-        const order = await ordersController.create(data, token);
+      async ({ ordersController, orderDraftStatus }) => {
+        const { id } = await orderDraftStatus(); // получаем созданный заказ в статусе Draft
 
-        createdCustomerIds.push(customer._id);
-        createdProductIds.push(product._id);
-        createdOrderIds.push(order.body.Order._id);
-
-        const response = await ordersController.getByID(order.body.Order._id, token)
+        const response = await ordersController.getByID(id, token);
         validateResponse(response, STATUS_CODES.OK, true, null);
+        validateSchema(getOrderByIDResponseSchema, response.body.Order);
       });
   });
 
     test.describe('Negative', () => {
       test('404 Not Found - Get order by invalid id',
         { tag: [TAGS.API, TAGS.ORDERS, TAGS.REGRESSION] },
-        async ({ ordersController, customersApiService, productsApiService }) => {
-          const customer = await customersApiService.createCustomer(token);
-          const product = await productsApiService.create(token);
-          const data = {
-            customer: customer._id,
-            products: [product._id],
-          };
-          const order = await ordersController.create(data, token);
-          await ordersController.delete(order.body.Order._id, token);
-          const orderId = order.body.Order._id;
+        async ({ ordersController, orderDraftStatus }) => {
+          const id = MOCK_PRODUCT_ONE._id;
 
-          createdCustomerIds.push(customer._id);
-          createdProductIds.push(product._id);
-
-          const response = await ordersController.getByID(orderId, token)
-          validateResponse(response, STATUS_CODES.NOT_FOUND, false, `Order with id '${orderId}' wasn't found`);
+          const response = await ordersController.getByID(id, token);
+          validateResponse(response, STATUS_CODES.NOT_FOUND, false, `Order with id '${id}' wasn't found`);
         });
 
       test('401 Unauthorized - Get order by id without token',
         { tag: [TAGS.API, TAGS.ORDERS, TAGS.REGRESSION] },
-        async ({ ordersController, customersApiService, productsApiService }) => {
-          const customer = await customersApiService.createCustomer(token);
-          const product = await productsApiService.create(token);
-          const data = {
-            customer: customer._id,
-            products: [product._id],
-          };
-          const order = await ordersController.create(data, token);
-          const orderId = order.body.Order._id;
+        async ({ ordersController, orderDraftStatus }) => {
+          const { id } = await orderDraftStatus();
 
-          createdCustomerIds.push(customer._id);
-          createdProductIds.push(product._id);
-          createdOrderIds.push(orderId);
-
-          const response = await ordersController.getByID(orderId, '');
-          validateResponse(response, STATUS_CODES.UNAUTHORIZED, false, 'Not authorized');
+          const response = await ordersController.getByID(id, '');
+          validateResponse(response, STATUS_CODES.UNAUTHORIZED, false, ERROR_MESSAGES.NOT_AUTHORIZED);
         });
 
       test('401 Unauthorized - Get order by id with invalid token',
         { tag: [TAGS.API, TAGS.ORDERS, TAGS.REGRESSION] },
-        async ({ ordersController, customersApiService, productsApiService }) => {
-          const customer = await customersApiService.createCustomer(token);
-          const product = await productsApiService.create(token);
-          const data = {
-            customer: customer._id,
-            products: [product._id],
-          };
-          const order = await ordersController.create(data, token);
-          const orderId = order.body.Order._id;
+        async ({ ordersController, orderDraftStatus }) => {
+          const { id } = await orderDraftStatus();
 
-          createdCustomerIds.push(customer._id);
-          createdProductIds.push(product._id);
-          createdOrderIds.push(orderId);
-
-          const response = await ordersController.getByID(orderId, 'Invalid token');
-          validateResponse(response, STATUS_CODES.UNAUTHORIZED, false, 'Invalid access token');
+          const response = await ordersController.getByID(id, 'Invalid token');
+          validateResponse(response, STATUS_CODES.UNAUTHORIZED, false, ERROR_MESSAGES.INVALID_ACCESS_TOKEN);
         });
     });
 });
